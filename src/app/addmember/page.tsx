d144ffb5777f8cf24179c0e0ec3a0feb6afecab2
toshakorn <123184092+toshakorn@users.noreply.github.com>
2023-11-06 02:53:39 +0700

    member

--- src/app/addmember/page.tsx
+++ src/app/addmember/page.tsx
@@ -1,8 +1,71 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import Swal from "sweetalert2";
 
 type Props = {};
 
 const AddMember = (props: Props) => {
+  const [name, setName] = useState("");
+  const [idCard, setIdCard] = useState("");
+
+  const handleAddMember = () => {
+    const data = {
+      name: name,
+      idnumber: idCard,
+    };
+  
+    // ทำการตรวจสอบว่ามีผู้ใช้นี้อยู่ในฐานข้อมูลหรือไม่
+    fetch("http://localhost:8081/user", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.status == 205) {
+          // ถ้ามีผู้ใช้นี้อยู่แล้ว
+          Swal.fire({
+            icon: 'warning',
+            title: 'มี member คนนี้อยู่แล้ว',
+            text: 'โปรดตรวจสอบอีกครั้ง',
+          });
+          console.log("มีผู้ใช้นี้อยู่แล้ว");
+        } else {
+          // ถ้ายังไม่มีผู้ใช้นี้ในฐานข้อมูล
+          // ทำการ POST ข้อมูลใหม่
+          fetch("http://localhost:8081/user", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+          })
+            .then((response) => {
+              if (response.ok) {
+                Swal.fire({
+                  position: 'top-end',
+                  icon: 'success',
+                  title: 'Your work has been saved',
+                  showConfirmButton: false,
+                  timer: 1500,
+                });
+                console.log("เพิ่มสมาชิกสำเร็จ");
+              } else {
+                console.error("การเพิ่มสมาชิกล้มเหลว");
+              }
+            })
+            .catch((error) => {
+              console.error("เกิดข้อผิดพลาดในการเชื่อมต่อเครือข่าย: " + error);
+            });
+        }
+      })
+      .catch((error) => {
+        console.error("เกิดข้อผิดพลาดในการตรวจสอบผู้ใช้: " + error);
+      });
+  };
+  
   return (
     <div>
       <div className="flex flex-col items-center">
@@ -12,14 +75,29 @@ const AddMember = (props: Props) => {
             <div className="flex flex-col justify-center h-[400px] ml-[150px]">
               <div>
                 <p className="text-[35px]">ชื่อ นามสกุล</p>
-                <input type="text" className="border-2 border-black text-[25px]" />
+                <input
+                  type="text"
+                  className="border-2 border-black text-[25px]"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
               </div>
               <div>
                 <p className="text-[35px]">เลขบัตรประจำตัวประชาชน</p>
-                <input type="text" className="border-2 border-black text-[25px]" />
+                <input
+                  type="text"
+                  className="border-2 border-black text-[25px]"
+                  value={idCard}
+                  onChange={(e) => setIdCard(e.target.value)}
+                />
               </div>
             </div>
-            <button className="w-[350px] rounded-[8px] h-[45px] bg-[#35BF3B] text-white ml-[400px] text-[35px]">เพิ่มสมาชิก</button>
+            <button
+              className="w-[350px] rounded-[8px] h-[45px] bg-[#35BF3B] text-white ml-[400px] text-[35px]"
+              onClick={handleAddMember}
+            >
+              เพิ่มสมาชิก
+            </button>
           </div>
         </div>
       </div>
--- src/app/listmember/page.tsx
+++ src/app/listmember/page.tsx
@@ -1,19 +1,165 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios"; // นำเข้า Axios
+import Swal from "sweetalert2";
+import { Button, Form, Input, Modal } from "antd";
 
 type Props = {};
+type User = {
+  // กำหนดรูปแบบข้อมูลหนังสือ
+  _id: string; // เพิ่มฟิลด์ _id
+  name: string;
+  idnumber: string;
+};
 
 const ListMember = (props: Props) => {
+  const [userData, setUserData] = useState<User[]>([]);
+  const [editMember, setEditMember] = useState<User | null>(null);
+  const [editMemberData, setEditMemberData] = useState<User | null>(null);
+  const [editModalVisible, setEditModalVisible] = useState(false);
+  const [name, setName] = useState<string>("");
+  const [idnumber, setIdnumber] = useState<string>("");
+
+  useEffect(() => {
+    // ใช้ Axios ในการดึงข้อมูลจาก API
+    fetchData();
+  }, []);
+
+  const handleEdit = (member: User) => {
+    setEditMemberData(member);
+    console.log(member);
+    setName(member.name); // กำหนดค่าเริ่มต้นของ name
+    setIdnumber(member.idnumber); // กำหนดค่าเริ่มต้นของ idnumber
+    setEditModalVisible(true);
+  };
+
+  const handleEditSubmit = () => {
+    if (editMemberData) {
+      const editedData = {
+        name: name,
+        idnumber: idnumber,
+      };
+      console.log(editedData);
+      axios
+        .put(`http://localhost:8081/user/${editMemberData._id}`, editedData)
+        .then(() => {
+          Swal.fire("แก้ไขสมาชิกสำเร็จ", "", "success");
+          fetchData();
+          setEditModalVisible(false);
+        })
+        .catch((error) => {
+          console.error("เกิดข้อผิดพลาดในการแก้ไขสมาชิก: ", error);
+          Swal.fire("เกิดข้อผิดพลาด", "ไม่สามารถแก้ไขสมาชิกได้", "error");
+        });
+    }
+  };
+
+  const handleEditCancel = () => {
+    setEditMemberData(null);
+    setEditModalVisible(false); // ปิด Modal โดยเรียก setEditModalVisible(false)
+    console.log("HI");
+  };
+
+  const handleDelete = (index: number) => {
+    Swal.fire({
+      title: "คุณแน่ใจหรือไม่?",
+      text: "คุณต้องการลบสมาชิกนี้หรือไม่?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "ใช่, ลบ!",
+      cancelButtonText: "ยกเลิก",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        const userId = userData[index]._id;
+        axios
+          .delete(`http://localhost:8081/user/${userId}`)
+          .then(() => {
+            Swal.fire("ลบสมาชิกสำเร็จ", "", "success");
+            fetchData();
+          })
+          .catch((error) => {
+            console.error("เกิดข้อผิดพลาดในการลบสมาชิก: ", error);
+            Swal.fire("เกิดข้อผิดพลาด", "ไม่สามารถลบสมาชิกได้", "error");
+          });
+      }
+    });
+  };
+
+  const fetchData = () => {
+    axios
+      .get("http://localhost:8081/user")
+      .then((response) => {
+        // สั่ง setBooks เพื่อเซ็ตข้อมูลหนังสือ
+        setUserData(response.data);
+      })
+      .catch((error) => {
+        console.error("เกิดข้อผิดพลาดในการดึงข้อมูล: ", error);
+      });
+  };
+
   return (
     <div className="flex flex-col items-center">
       <h1 className="mt-[5px] text-[35px]">รายการสมาชิก</h1>
       <div className="w-[1250px] border-2 border-black rounded-[18px] mt-[5px] h-[800px] bg-[#FFA3E0] ml-[50px] flex flex-col items-center">
-        <div className="w-[1150px] h-[45px] bg-white mt-[35px] border-2 border-black flex justify-around items-center">
-          <p>ลำดับ</p>
-          <p>ชื่อ</p>
-          <p>รหัสบัตรประชาชน</p>
-          <p>จัดการ</p>
+        <div className="w-[1150px] h-[45px] bg-white mt-[35px] border-2 border-black flex items-center">
+          <p className="w-[330px]">ลำดับ</p>
+          <p className="w-[330px]">ชื่อ</p>
+          <p className="w-[330px]">รหัสบัตรประชาชน</p>
+          <p className="w-[330px]">จัดการ</p>
         </div>
-        <div className="w-[1150px] h-[600px] bg-white mt-2 border-2 border-black"></div>
+        <div className="w-[1150px] h-[600px] bg-white mt-2 border-2 border-black overflow-auto">
+          {userData.map((item: any, index: number) => (
+            <div key={index} className="flex items-center mt-3">
+              <p className="w-[280px] ml-[15px]">{index + 1}</p>
+              <p className="w-[280px]">{item.name}</p>
+              <p className="w-[280px]">{item.idnumber}</p>
+              <div>
+                <button
+                  className="bg-yellow-400 mr-4 w-[45px] h-[35px] rounded-[7px]"
+                  onClick={() => handleEdit(item)}
+                >
+                  แก้ไข
+                </button>
+
+                <button
+                  onClick={() => handleDelete(index)}
+                  className="ml-4 bg-red-600 w-[45px] h-[35px] rounded-[7px]"
+                >
+                  ลบ
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+        <Modal
+          title="แก้ไขสมาชิก"
+          visible={editModalVisible}
+          onOk={handleEditSubmit}
+          onCancel={handleEditCancel}
+        >
+          <div>
+            <p>ชื่อหนังสือ</p>
+            <input
+              className="border-2 border-black"
+              type="text"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
+          <div>
+            <p>รหัสบัตรประชาชน</p>
+            <input
+              className="border-2 border-black"
+              type="text"
+              name="idnumber"
+              value={idnumber}
+              onChange={(e) => setIdnumber(e.target.value)}
+            />
+          </div>
+        </Modal>
       </div>
     </div>
   );
